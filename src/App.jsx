8be9293650b7a2c94b5f5d61123152412dbcc777
Alessandro Romano <ale.romano.esterno@almaviva.it>
2024-10-10 14:51:28 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faqs from "./sections/Faqs.jsx";
+import Download from "./sections/Download.jsx";
+import Testimonals from "./sections/Testimonals.jsx";
+import Footer from "./sections/Footer.jsx";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faqs />
+      <Testimonals />
+      <Download />
+      <Footer />
+    </main>
+  );
+};
+export default App;
